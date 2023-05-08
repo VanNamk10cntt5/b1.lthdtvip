b4fb2cee91ced8fae8fa13698ada2406c02dcc88
Van_Nam_CNTT5_10 <125737934+VanNamk10cntt5@users.noreply.github.com>
2023-05-08 14:07:04 +0700

    Create 2.lophocphan.java

---
+++ 2.lophocphan.java
@@ -0,0 +1,20 @@
+package bai2;
+
+public class LopHocPhan {
+	public String maLHP;
+	public String tenLHP;
+	public String tenGV;
+	public String thongtinLopHoc;
+	public SinhVien dsSV;
+	
+	public LopHocPhan(String maLHP,String tenLHP,String tenGV,String thongtinLopHoc,SinhVien dsSV) {
+		this.maLHP = maLHP;
+		this.maLHP = maLHP;
+		this.tenGV = tenGV;
+		this.thongtinLopHoc = thongtinLopHoc;
+		this.dsSV = dsSV;
+	}
+	
+	
+	
+}
