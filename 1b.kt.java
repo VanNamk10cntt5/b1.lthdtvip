52545f069a36300e37bb68ba048d2c7bb97f2ccd
Van_Nam_CNTT5_10 <125737934+VanNamk10cntt5@users.noreply.github.com>
2023-05-08 14:00:34 +0700

    Create 1b.kt.java

---
+++ 1b.kt.java
@@ -0,0 +1,14 @@
+package bai1b;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Kiemtra {
+	public static void main(String[] args) {
+		ArrayList<HangThucPham> ds = new ArrayList<HangThucPham>();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		LocalDate date =LocalDate new
+		ds.add("001","Gao",10000000,10/07/2018,10/07/2018);
+	}
+}
